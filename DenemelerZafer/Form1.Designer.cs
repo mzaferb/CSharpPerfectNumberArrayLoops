3c8121256e77b681578863f6483050441436e710
mzaferb <bildik@yandex.com>
2023-10-04 13:49:06 +0300

    Add project files.

---
+++ DenemelerZafer/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace DenemelerZafer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            groupBox1 = new GroupBox();
+            textBox2 = new TextBox();
+            listBox2 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            button3 = new Button();
+            label4 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 251);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Perfect Numbers";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(6, 61);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 184);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(211, 58);
+            button2.Name = "button2";
+            button2.Size = new Size(96, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Add";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(6, 286);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(150, 69);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "Finds the perfect numbers between 2 and textBox.Text";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(listBox1);
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(9, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(163, 391);
+            groupBox1.TabIndex = 7;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(211, 25);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(96, 27);
+            textBox2.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 20;
+            listBox2.Location = new Point(211, 93);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(96, 164);
+            listBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(211, 269);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 20);
+            label1.TabIndex = 10;
+            label1.Text = "Min :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(211, 298);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 11;
+            label2.Text = "Max :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(211, 327);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 20);
+            label3.TabIndex = 12;
+            label3.Text = "Sum :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(254, 269);
+            label5.Name = "label5";
+            label5.Size = new Size(17, 20);
+            label5.TabIndex = 14;
+            label5.Text = "0";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(254, 298);
+            label6.Name = "label6";
+            label6.Size = new Size(17, 20);
+            label6.TabIndex = 15;
+            label6.Text = "0";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(254, 327);
+            label7.Name = "label7";
+            label7.Size = new Size(17, 20);
+            label7.TabIndex = 16;
+            label7.Text = "0";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(211, 359);
+            button3.Name = "button3";
+            button3.Size = new Size(96, 29);
+            button3.TabIndex = 18;
+            button3.Text = "Find";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(231, 391);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 20);
+            label4.TabIndex = 19;
+            label4.Text = "ARRAY";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(440, 450);
+            Controls.Add(label4);
+            Controls.Add(button3);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox2);
+            Controls.Add(textBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(button2);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+        private GroupBox groupBox1;
+        private TextBox textBox2;
+        private ListBox listBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button button3;
+        private Label label4;
+    }
+}
---
+++ DenemelerZafer/Form1.cs
@@ -0,0 +1,65 @@
+﻿namespace DenemelerZafer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Perfect Number
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime now1 = DateTime.Now;
+
+            listBox1.Items.Clear();
+            int j, sum;
+            int number = Convert.ToInt32(textBox1.Text);
+            j = 2;
+
+            while (j <= number)
+            {
+                sum = 0;
+                for (int i = 1; i <= j / 2; i++)
+                {
+                    if (j % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+
+                if (sum == j)
+                {
+                    listBox1.Items.Add(j);
+                }
+                j += 2;
+            }
+            DateTime now2 = DateTime.Now;
+            MessageBox.Show(Convert.ToString(now2 - now1));
+        }
+        //Perfect Number
+
+        //Creating an array by taking values ​​from listBox and finding the min, max and sum. 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox2.Text);
+            textBox2.Clear();
+            textBox2.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] numbers = new int[listBox2.Items.Count];
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = Convert.ToInt32(listBox2.Items[i]);
+            Array.Sort(numbers);
+            label5.Text = numbers[0].ToString();
+            label6.Text = numbers[numbers.Length - 1].ToString();
+
+            int sum = 0;
+            foreach (int j in numbers)
+                sum += j;
+            label7.Text = sum.ToString();
+        }
+        //Creating an array by taking values ​​from listBox and finding the min, max and sum.
+    }
+}
